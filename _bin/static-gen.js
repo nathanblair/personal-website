9bc79dbfaad28fd3368f17ec98e184fac4c03cae
Nathan Blair <nathan.t.blair@outlook.com>
2017-04-11 06:43:26 -0400

    Rewriting BlogWalk

--- _bin/static-gen.js
+++ _bin/static-gen.js
@@ -1,46 +1,29 @@
 const fs 						= require('fs');
 const pth						= require('path');
 const sg						= require('./modules/SG.js');
-const cwd						= process.cwd();
 var categories 					= ['tech', 'personal'];
 
+var blogFiles					= [];
 var blogPreviews				= [];
 
 
-function TestAndExtractFile_Async(err, fileStats) {
-	// If there was an error in testing the file
-	if( err ) { console.log( err.message ); } else {
-		// Check that the file found is actually a directory
-		if( fileStats.isDirectory() ) {
-			// Local save of the full file name
-			var fileName = pth.join( filePath, 'index.html' );
-			// Attempt to read the file
-			fs.readFile( fileName, 'utf8', ( err, contents ) => {
-				// Check that no error was made in reading the file
-				if( err ) { console.log( err.message ); } else {
-					// Call to write the articles for that file
-					blogPreviews[fileName] = sg.GetBlogPreview( fileName, contents, categories[cat] );
-				}
-			})
-		}
-	}
-}
+
 
 // Main call for module functionality
-function WalkBlogDirectory( categories ) {
-	// Iterate through possible categories
-	for( cat = 0; cat <= categories.length - 1; cat++ ) {
-		// For the current iteration, store a testable directory
-		var fileCat = pth.join( 'blog', categories[cat] );
-
-		// Get the files/folders in the testable directory
-		fs.readdirSync( fileCat ).forEach( function CheckBlog(entry, ind) {
-			console.log(entry);
-			// Local save of the full file directory
-			var filePath = pth.join( fileCat, entry );
-			fs.stat( filePath, TestAndExtractFile_Async ); 
-		});
-	}
+function WalkBlogDirectory(dir, inBlog, foundArticles) {
+	fs.readdir(dir, (err, files) => {
+		files.forEach( (entry, ind) => {
+			var blogDir = pth.join(dir, entry);
+			fs.stat( blogDir, (err, fileStats) => {
+				if (fileStats.isDirectory()) {
+					if (categories.includes(entry)) { WalkBlogDirectory(blogDir, true, false); }
+					else if (inBlog) { WalkBlogDirectory(blogDir, false, true); }
+				} else if (foundArticles) {
+					blogFiles.push(blogDir);
+				}
+			})
+		})
+	})
 }
 
-WalkBlogDirectory( categories );
+WalkBlogDirectory( "blog", false, false );
