1955800096c9a4d5873eebea9705204b1bf39f26
Nathan Blair <nathan.t.blair@outlook.com>
2019-04-11 11:26:02 -0400

    Tabs -> spaces
    Included details of position at BlueQuartz

--- _bin/server.js
+++ _bin/server.js
@@ -6,50 +6,50 @@ const port				= 3000;
 
 // Get the URI - has its own function to allow recursivity
 function deliverURI(uri, res) {
-	// Try accessing the file
-	fs.readFile(uri, (err, contents) => {
-		if (err) {
-			// Test if the url was requested of a root directory and populate a default index file if it was
-			if ((uri[uri.length-1]) == '/') {
-				try {
-					var tmpUri = uri + 'index.html';
-					fs.accessSync(tmpUri);
-					uri = tmpUri;
-				} catch (e) {
-					try {
-						console.log('Could not find file ' + tmpUri);
-						var tmpUri = uri + 'index';
-						fs.accessSync(tmpUri);
-						uri = tmpUri;
-					} catch (e) {
-						res.statusCode = 404;
-						res.end('Bad request\nNo file given or file not found!');
-					}
-				}
-				deliverURI(uri, res);
-			}
-		} else {
-			let mimeType = ''
-			res.statusCode = 200;
-			res.end(contents);
-		}
-	})
+    // Try accessing the file
+    fs.readFile(uri, (err, contents) => {
+        if (err) {
+            // Test if the url was requested of a root directory and populate a default index file if it was
+            if ((uri[uri.length-1]) == '/') {
+                try {
+                    var tmpUri = uri + 'index.html';
+                    fs.accessSync(tmpUri);
+                    uri = tmpUri;
+                } catch (e) {
+                    try {
+                        console.log('Could not find file ' + tmpUri);
+                        var tmpUri = uri + 'index';
+                        fs.accessSync(tmpUri);
+                        uri = tmpUri;
+                    } catch (e) {
+                        res.statusCode = 404;
+                        res.end('Bad request\nNo file given or file not found!');
+                    }
+                }
+                deliverURI(uri, res);
+            }
+        } else {
+            let mimeType = ''
+            res.statusCode = 200;
+            res.end(contents);
+        }
+    })
 }
 
 // Callback that handles the request and serves the response
 const requestHandler = (req, res) => {
-	var fullURI = '.' + req.url;
-	// console.log(`Request was made for ${fullURI}`)
+    var fullURI = '.' + req.url;
+    // console.log(`Request was made for ${fullURI}`)
 
-	deliverURI(fullURI, res);
+    deliverURI(fullURI, res);
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
-	if (err) {
-		return console.log('something bad happened', err);
-	}
+    if (err) {
+        return console.log('something bad happened', err);
+    }
 
-	console.log(`server is listening on ${port}`);
+    console.log(`server is listening on ${port}`);
 })
--- scripts/page.js
+++ scripts/page.js
@@ -2,10 +2,10 @@ var pathName = ''
 $.get('/head.html', data => { $('head').append(data) })
 
 $.get('/nav.html', data => {
-	$('main').before(data)
-	pathName = window.location.pathname.replace(/^\/|\/$/g, '')
-	window.document.title = window.document.title + ' ' + pathName
-	$('.name-logo > span').html('you-are@<i>nathanblair.rocks</i> : ~/' + pathName + ' $ <span class="caret">&#9608;</span>')
+    $('main').before(data)
+    pathName = window.location.pathname.replace(/^\/|\/$/g, '')
+    window.document.title = window.document.title + ' ' + pathName
+    $('.name-logo > span').html('you-are@<i>nathanblair.rocks</i> : ~/' + pathName + ' $ <span class="caret">&#9608;</span>')
 })
 $.get('/footer.html', data => { $('footer').html(data) })
 
