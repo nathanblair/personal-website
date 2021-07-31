4f1960e7a4cfa8f6b2c75dc7d9021cd4a403eb55
Nathan Blair <engineer.nblair@gmail.com>
2021-07-30 23:44:55 -0400

    still have a lot of work to do to clean up resume
    make it look great on mobile viewport
    
    fix formatting and layout of open source contributions
    
    maybe add something about developing a terraform provisioner for local kubernetes?

--- src/pages/Resume.svelte
+++ src/pages/Resume.svelte
@@ -39,6 +39,64 @@
   <div class="content">
     <section class="job">
       <div class="titlebar">
+        <span class="name">Genetesis</span><br />
+        <hr id="titleDivider" />
+        <span class="detail">DevOps Engineer</span>
+      </div>
+      <div class="detailbar">
+        <span class="location">Mason, OH</span><br id="detailBreak" />
+        <hr id="detailDivider" />
+        <span class="date">November 2020 - Present</span>
+      </div>
+      <ul class="workdesc">
+        <li>
+          Developed installer programs for desktop apps using Go and managed
+          their CI/CD automation
+        </li>
+        <li>
+          Automated bare-metal machine image generation and deployment to
+          embedded devices
+        </li>
+        <li>
+          Migrated AWS EKS instances running company’s microservices from manual
+          provisioning to automated provisioning using Terraform
+        </li>
+      </ul>
+    </section>
+    <section class="job">
+      <div class="titlebar">
+        <span class="name">Ascendum</span><br />
+        <hr id="titleDivider" />
+        <span class="detail">DevOps Engineer</span>
+      </div>
+      <div class="detailbar">
+        <span class="location">Blue Ash, OH</span><br id="detailBreak" />
+        <hr id="detailDivider" />
+        <span class="date">May 2019 - November 2020</span>
+      </div>
+      <ul class="workdesc">
+        <li>
+          Created and managed automation pipelines for iOS (including
+          codesigning) and Android apps
+        </li>
+        <li>
+          Employed Google Firebase and Google Cloud Platform for provisioning
+          mobile application resources like notification services and cloud
+          databases
+        </li>
+        <li>
+          Assisted clients in setting up AWS resources like AWS Lambda,
+          DynamoDB, AWS RDS (MySQL), various EC2 and other container and virtual
+          hosts
+        </li>
+        <li>
+          Utilized Azure and Azure DevOps to host wide array of apps developed
+          across the company
+        </li>
+      </ul>
+    </section>
+    <section class="job">
+      <div class="titlebar">
         <span class="name">BlueQuartz</span><br />
         <hr id="titleDivider" />
         <span class="detail">Junior Computer Programmer III</span>
@@ -46,7 +104,7 @@
       <div class="detailbar">
         <span class="location">Springboro, OH</span><br id="detailBreak" />
         <hr id="detailDivider" />
-        <span class="date">December 2018 - Present</span>
+        <span class="date">December 2018 - May 2019</span>
       </div>
       <ul class="workdesc">
         <li>Created data/image Grayscale filter and unit test</li>
@@ -62,14 +120,14 @@
     </section>
     <section class="job">
       <div class="titlebar">
-        <span class="name">iTSpeeX (Contractor)</span><br />
+        <span class="name">iTSpeeX (Contract)</span><br />
         <hr id="titleDivider" />
         <span class="detail">QA Department Head</span>
       </div>
       <div class="detailbar">
         <span class="location">Mason, OH</span><br id="detailBreak" />
         <hr id="detailDivider" />
-        <span class="date">July 2018 - December 2018</span>
+        <span class="date">June 2018 - December 2018</span>
       </div>
       <ul class="workdesc">
         <li>
@@ -79,20 +137,6 @@
           Brought on and set up JIRA, Bitbucket, and Bamboo Server Editions
         </li>
         <li>Set up automated build and testing server using Bamboo</li>
-      </ul>
-    </section>
-    <section class="job">
-      <div class="titlebar">
-        <span class="name">iTSpeeX (Contractor)</span><br />
-        <hr id="titleDivider" />
-        <span class="detail">Software Engineer</span>
-      </div>
-      <div class="detailbar">
-        <span class="location">Mason, OH</span><br id="detailBreak" />
-        <hr id="detailDivider" />
-        <span class="date">June 2018 - July 2018</span>
-      </div>
-      <ul class="workdesc">
         <li>Developed C# Core architecture using SOLID principles</li>
         <li>
           Lead the integration of git version control and gitflow methodologies
@@ -100,223 +144,118 @@
         </li>
       </ul>
     </section>
+  </div>
+</section>
+<section>
+  <h1>Open Source</h1>
+  <div class="content">
     <section class="job">
       <div class="titlebar">
-        <span class="name"
-          >Wright Patt AFB Education Outreach Office (Contractor)</span
-        ><br />
+        <span class="name">NodeJS</span><br />
         <hr id="titleDivider" />
-        <span class="detail">Project Engineer</span>
+        <span class="detail">Contributor</span>
       </div>
       <div class="detailbar">
-        <span class="location">Dayton, OH</span><br id="detailBreak" />
+        <span class="location" /><br id="detailBreak" />
         <hr id="detailDivider" />
-        <span class="date">November 2017 - June 2018</span>
+        <span class="date" />
       </div>
       <ul class="workdesc">
         <li>
-          Coordinated projects for multiple programs within the Education
-          Outreach office at WPAFB including FIRST Lego League, WOW!, and LEGACY
-        </li>
-        <li>
-          Developed Inventory Tracking System web-app using Node.js backend and
-          deployed for multiple program use
-        </li>
-        <li>
-          Assisted technical team for FIRST events at Wright State Nutter Center
+          Modifications to build script to allow compilation using clang on
+          Alpine Linux
         </li>
       </ul>
     </section>
     <section class="job">
       <div class="titlebar">
-        <span class="name">Innomark Communications (Contractor)</span><br />
+        <span class="name">Alpine Linux</span><br />
         <hr id="titleDivider" />
-        <span class="detail">Junior Software Developer</span>
+        <span class="detail">Contributor</span>
       </div>
       <div class="detailbar">
-        <span class="location">Fairfield, OH</span><br id="detailBreak" />
+        <span class="location" /><br id="detailBreak" />
         <hr id="detailDivider" />
-        <span class="date">October 2017 - November 2017</span>
+        <span class="date" />
       </div>
       <ul class="workdesc">
         <li>
-          Spearheaded development on commercial web-app allowing customers to
-          build store-specific signage packaging
-        </li>
-        <li>Supported development of legacy software for retail web-apps</li>
-        <li>
-          Assisted in IT Operations team in managing Windows Server 2012 and AD
-          services
-        </li>
-        <li>
-          Chosen to champion Agile development approach within developer team
-          and integrate Visual Studio TFS Agile tools into development workflow
+          Issue reporting, debugging, and fixing for various pacakges including
+          nodejs and xkeyboard-config
         </li>
       </ul>
     </section>
     <section class="job">
       <div class="titlebar">
-        <span class="name">Berg Steel Pipe</span><br />
+        <span class="name">Void Linux</span><br />
         <hr id="titleDivider" />
-        <span class="detail">QA Process Engineer</span>
+        <span class="detail">Contributor</span>
       </div>
       <div class="detailbar">
-        <span class="location">Panama City, FL</span><br id="detailBreak" />
+        <span class="location" /><br id="detailBreak" />
         <hr id="detailDivider" />
-        <span class="date">August 2015 - April 2016</span>
+        <span class="date" />
       </div>
       <ul class="workdesc">
-        <li>
-          Overhauled quality reporting procedures to better track production and
-          Quality performance using API's for Minitab, MS Office, and MS SQL
-          Server
-        </li>
-        <li>
-          Advocated integration and accessibility of MS SQL Server and IBM
-          databases to dramatically decrease report generation times
-        </li>
-        <li>
-          Applied multi-disciplinary background to interface with multiple
-          departments and facilitate corporate homogeneity
-        </li>
+        <li>Maintainer for NodeJS early version 14</li>
       </ul>
     </section>
     <section class="job">
       <div class="titlebar">
-        <span class="name">Xtek Inc.</span><br />
+        <span class="name">Jester NPM Package</span><br />
         <hr id="titleDivider" />
-        <span class="detail">Metallurgical Engineer/Systems Engineer</span>
+        <span class="detail">Developer</span>
       </div>
       <div class="detailbar">
-        <span class="location">Cincinnati, OH</span><br id="detailBreak" />
+        <span class="location" /><br id="detailBreak" />
         <hr id="detailDivider" />
-        <span class="date">August 2013 - August 2014</span>
+        <span class="date" />
       </div>
       <ul class="workdesc">
-        <li>
-          Prepared metallurgical samples for microstructure evaluation and NDT
-          failure analysis
-        </li>
-        <li>Participated on work-orders for SAW/MIG weld procedures</li>
-        <li>
-          Developed below-the-hook (mechanical component) design software in
-          VB.NET to automate design and interface with SolidWorks
-        </li>
+        <li>Jester is an npm package that simplifies javascript unit testing</li>
       </ul>
     </section>
   </div>
 </section>
 <section>
-  <h1>Independent</h1>
-  <div class="content">
-    <section class="job">
-      <div class="titlebar">
-        <span class="name">Substitute Teacher Portal</span><br />
-        <hr id="titleDivider" />
-        <span class="detail">Developer</span>
-      </div>
-      <div class="detailbar">
-        <span class="location">Dayton, OH</span><br id="detailBreak" />
-        <hr id="detailDivider" />
-        <span class="date">January 2017 - August 2017</span>
-      </div>
-      <ul class="workdesc">
-        <li>
-          Created Python package on a Raspberry Pi that logs into online site
-          and scrapes HTML source
-        </li>
-        <li>
-          Developed SQLite backend database that manages whether jobs retrieved
-          from source are current, new, or inactive
-        </li>
-        <li>
-          Utilized Linux SMTP tool to send a formatted e-mail with details
-          regarding job status
-        </li>
-        <li>
-          Executed scrape and update operation periodically using Linux <i
-            >systemd</i
-          > timers
-        </li>
-      </ul>
-    </section>
-    <section class="job">
-      <div class="titlebar">
-        <span class="name">Android Sensor App</span><br />
-        <hr id="titleDivider" />
-        <span class="detail">Developer</span>
-      </div>
-      <div class="detailbar">
-        <span class="location">Dayton, OH</span><br id="detailBreak" />
-        <hr id="detailDivider" />
-        <span class="date">May 2015 - December 2017</span>
-      </div>
-      <ul class="workdesc">
-        <li>
-          Created an augmented reality application that utilizes on-board
-          Android MEMS sensors like the Gyroscope and Accelerometer using the C#
-          language
-        </li>
-        <li>
-          Applied knowledge of sensor fusion field to make a fluid, smooth
-          experience
-        </li>
-        <li>
-          Tracked and studied sensor input using MATLAB Mobile Device package
-          (mobiledev) and custom-build MATLAB library
-        </li>
-        <li>Utilized Unity game studio software for graphic development</li>
-      </ul>
-    </section>
-    <section class="job">
-      <div class="titlebar">
-        <span class="name">NASA ICED ACRUM</span><br />
-        <hr id="titleDivider" />
-        <span class="detail">Materials Engineer/Communications Lead</span>
-      </div>
-      <div class="detailbar">
-        <span class="location">Dayton, OH</span><br id="detailBreak" />
-        <hr id="detailDivider" />
-        <span class="date">August 2014 - May 2015</span>
-      </div>
-      <ul class="workdesc">
-        <li>
-          Organized communications between students at top-tier Universities
-          across US and NASA experts collaborating on project
-        </li>
-        <li>
-          Coordinated project management-level tasks across multiple departments
-          and disciplines
-        </li>
-        <li>
-          Designed procedure for synthesizing glass from asteroid regolith
-        </li>
-      </ul>
-    </section>
-    <section class="job">
-      <div class="titlebar">
-        <span class="name">van der Pauw PR MEMS Sensors</span><br />
-        <hr id="titleDivider" />
-        <span class="detail">Undergraduate Researcher</span>
-      </div>
-      <div class="detailbar">
-        <span class="location">Dayton, OH</span><br id="detailBreak" />
-        <hr id="detailDivider" />
-        <span class="date">January 2015 - May 2015</span>
-      </div>
-      <ul class="workdesc">
-        <li>Designed van der Pauw piezoresistive devices for MEMS packaging</li>
-        <li>
-          Applied numerical solutions from FEA by using numerical modeling
-          software (ANSYS)
-        </li>
-        <li>
-          Publication submitted with Mechanical Engineering Professor Dr. Ahsan
-          Mian
-        </li>
-      </ul>
-    </section>
-  </div>
+  <h1>Skills</h1>
+  <table id="skilltable">
+    <tr>
+      <th>Languages</th>
+      <th>Tools</th>
+      <th>Frameworks</th>
+    </tr>
+    <tr>
+      <td>Python</td>
+      <td>docker</td>
+      <td>React</td>
+    </tr>
+    <tr>
+      <td>Go</td>
+      <td>kubernetes</td>
+      <td>Angular</td>
+    </tr>
+    <tr>
+      <td>Rust</td>
+      <td>terraform</td>
+      <td>Svelte</td>
+    </tr>
+    <tr>
+      <td>C#</td>
+      <td>git</td>
+      <td>Vue</td>
+    </tr>
+    <tr>
+      <td>C++/Visual C++</td>
+      <td>aws/gcp/azure</td>
+      <td>NodeJS</td>
+    </tr>
+    <tr>
+      <td>JavaScript/TypeScript</td>
+      <td>Unreal Engine/Unity</td>
+      <td />
+    </tr>
+  </table>
 </section>
 <section>
   <h1>Coursework</h1>
@@ -369,56 +308,6 @@
       <td>Materials Characterization</td>
       <td>MATLAB for Engineers</td>
       <td />
-    </tr>
-  </table>
-</section>
-<section>
-  <h1>Skills</h1>
-  <table id="skilltable">
-    <tr>
-      <th>Techniques and Instruments</th>
-      <th>Programs</th>
-      <th>Languages</th>
-    </tr>
-    <tr>
-      <td>Scanning Electron Microscopy (SEM)</td>
-      <td>SolidWorks</td>
-      <td>C++/Visual C++</td>
-    </tr>
-    <tr>
-      <td>Transmission Electron Microscopy (TEM)</td>
-      <td>MATLAB</td>
-      <td>C#</td>
-    </tr>
-    <tr>
-      <td>Atomic Force Microscopy (AFM)</td>
-      <td>Mathematica</td>
-      <td>VB.NET</td>
-    </tr>
-    <tr>
-      <td>Reactive Ion Etching (RIE)</td>
-      <td>LabVIEW</td>
-      <td>Visual Basic for Applications (VBA)</td>
-    </tr>
-    <tr>
-      <td>Focused Ion Beam (FIB)</td>
-      <td>ANSYS</td>
-      <td>Java</td>
-    </tr>
-    <tr>
-      <td>Nuclear Magnetic Resonance (NMR) Spectroscopy</td>
-      <td>Visual Studio</td>
-      <td>JavaScript</td>
-    </tr>
-    <tr>
-      <td>Wet Chemical Etching</td>
-      <td>GitHub</td>
-      <td>HTML</td>
-    </tr>
-    <tr>
-      <td>Chemical Vapor Deposition (CVD)</td>
-      <td>Minitab</td>
-      <td>CSS</td>
     </tr>
   </table>
 </section>
