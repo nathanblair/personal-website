06341674aab18e49caad4c82fdd70929a2363ec7
Nathan Blair <engineer.nblair@gmail.com>
2021-08-03 08:16:41 -0400

    this gets me PRETTY close
    to having a generator function that asynchronously populates
    the list of blog articles
    
    we're able to pull blog articles from github
    
    Now we need to style the article better
    
    and make the article collapsible while showing
    and hiding the snippet as appropriate

--- src/blog.js
+++ src/blog.js
@@ -1,47 +1,106 @@
-const github_content_list_endpoint = "https://api.github.com"
+const cloud_host_endpoint = "https://api.github.com"
 const blog_path = "/repos/nathanblair/blog/contents/"
+const cloud_host_blog_list_endpoint = `${cloud_host_endpoint}${blog_path}`
 
-/** @typedef {Array<{name: string, path: string, download_url: string}>} GitHubPayload */
-/** @typedef {Array<{title: string, date: string, snippet: string, article: string}>} Blog */
+/**
+ * @typedef {Array<{
+ *  name: string,
+ *  path: string,
+ *  type: "file" | "dir" | "symlink" | "submodule",
+ *  download_url: string
+ * }>} Payload
+ */
+
+/** @typedef {[string, string, string]} ArticleDateArray */
+
+/**
+ * @typedef {{
+ *  file_name: string,
+ *  date: ArticleDateArray,
+ *  content: string
+ * }} Article
+ */
+
+export const DateMap = Object.freeze({
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+})
+
+export function default_filter() {
+  const date = new Date()
+  // @ts-ignore
+  const month = DateMap[date.getMonth()]
+
+  return `${date.getFullYear()}/${month}`
+}
 
 /** @param {string} url */
-export async function download_blog_article(url) {
+async function download_blog_article(url) {
   return (await fetch(url)).text()
 }
 
-/** Fetch blog articles from the cloud host */
-async function fetch_blog_articles() {
+/**
+ * Fetch blog articles from the cloud host
+ *
+ * A filter can narrow results to a single year, a single month, or a single day
+ * and should return them in a form of `${year}/${month}/${day}, omitting
+ * whichever parameters are not needed
+ *
+ * To return articles for a time range, call this function appropriately over
+ * the necessary range
+ *
+ * @param {() => string} filter
+ *
+ * @returns {AsyncIterable<Article>}
+ */
+export async function* fetch_blog_articles(filter) {
   /** @type {Response} */
   let response
 
   try {
-    response = await fetch(`${github_content_list_endpoint}${blog_path}`)
+    response = await fetch(`${cloud_host_blog_list_endpoint}${filter()}`)
   } catch (err) {
     console.error(err)
-    return []
+    return
   }
 
-  /** @type {GitHubPayload} */
+  /** @type {Payload} */
   let body
   try {
     body = await response.json()
   } catch (err) {
     console.error(err)
-    return []
+    return
   }
 
-  /** @type {GitHubPayload} */
-  const articles = []
-
   for (const each_entry of body) {
-    switch (each_entry.name) {
-      case "old":
-      case ".gitignore":
-        break
-
-      default:
+    switch (each_entry.type) {
+      case "dir":
+        yield* fetch_blog_articles(() => each_entry.path)
+        return
+      case "file":
+        const date_array = each_entry.path.split("/")
+        date_array.pop()
+        /** @type {Article} */
+        let article
         try {
-          articles.push(each_entry)
+          article = {
+            file_name: each_entry.name,
+            // @ts-ignore
+            date: date_array,
+            content: await download_blog_article(each_entry.download_url),
+          }
+          yield article
         } catch (err) {
           console.error(err)
           continue
@@ -49,34 +108,4 @@ async function fetch_blog_articles() {
         break
     }
   }
-
-  return articles
-}
-
-export async function parse_blog_articles() {
-  // TODO extract the blog title, date, and generate an excerpt
-  const article_payload = await fetch_blog_articles()
-  /** @type {Blog} */
-  const articles = []
-
-  const parser = new DOMParser()
-
-  for (const each_article_payload of article_payload) {
-    const article_html = await download_blog_article(
-      each_article_payload.download_url
-    )
-
-    const article_element = parser.parseFromString(article_html, "text/html")
-
-    articles.push({
-      title:
-        article_element.querySelector("article > header > h1")?.innerHTML || "",
-      date:
-        article_element.querySelector("article > header > h2")?.innerHTML || "",
-      snippet: article_element.querySelector("article > p")?.innerHTML || "",
-      article: article_html,
-    })
-  }
-
-  return articles
 }
