376137e4a873c95bc00bafea3d4979725e26ba39
Nathan Blair <engineer.nblair@gmail.com>
2021-08-03 09:42:31 -0400

    this gets us a decent toggleable blog page
    the articles themselves are still ugly as sin but they will need to stay that way
    until I can find some time to write some longer ones and see how they look
    
    We also now have a FIXME for how we're going to be able to navigate
    directly to invididual blog articles using their fragments
    
    Idea for solution is posted in src

--- src/components/BlogArticle.svelte
+++ src/components/BlogArticle.svelte
@@ -7,13 +7,31 @@
   /** @type {import("../blog.js").ArticleDateArray} */
   export let date
 
+  /** @type {string} */
+  export let content
+
   /** @type {HTMLDivElement} */
   let content_accessor
+
+  /** @type {HTMLParagraphElement} */
+  let snippet_accessor
+
+  /** @type {HTMLAnchorElement} */
+  let article_link
 
   /** @type {string} */
   let snippet
 
+  let toggle = true
+
   let id = blog_file_name.replace(".html", "")
+
+  // FIXME How can we implement a direct link to page articles using fragments
+  // if the article list is only loaded dynamically?
+  // We would have to put embed the date parameters into the fragment
+  // and then whenever the blog page loads, check the url fragment to see if
+  // the date parameters are in there, and if they are, use those to construct
+  // the filter passed when the page loads
 
   /**
    * Parse the file name to strip out the date and title
@@ -23,24 +41,84 @@
     return blog_title
   }
 
+  /**
+   * Callback for clicking the article "link"
+   *
+   * @param {Event} e
+   */
+  function toggle_article_expansion(e) {
+    if (toggle) {
+      snippet_accessor.classList.add("hidden")
+      content_accessor.classList.remove("hidden")
+    } else {
+      snippet_accessor.classList.remove("hidden")
+      content_accessor.classList.add("hidden")
+
+      history.replaceState("", "", "/blog")
+      e.preventDefault()
+    }
+    toggle = !toggle
+  }
+
   onMount(() => {
     snippet = content_accessor.firstElementChild?.innerHTML || ""
   })
 </script>
 
-<article {id}>
-  <header>
-    <h1>{parse_blog_file_name()}</h1>
-    <h2>{date[2]} {date[1]} {date[0]}</h2>
-  </header>
-  <div class="article-content" bind:this={content_accessor}>
-    <slot />
-  </div>
-  <p class="snippet">{snippet}</p>
-</article>
+<a
+  href={window.location.href + "#" + id}
+  on:click={toggle_article_expansion}
+  bind:this={article_link}
+>
+  <article {id}>
+    <header>
+      <h1>{parse_blog_file_name()}</h1>
+      <h2>{date[2]} {date[1]} {date[0]}</h2>
+    </header>
+    <div class="article-content hidden" bind:this={content_accessor}>
+      {@html content}
+    </div>
+    <p class="snippet" bind:this={snippet_accessor}>{snippet}</p>
+  </article>
+</a>
 
 <style>
-  .article-content {
-    display: none;
+  a {
+    text-decoration: none;
+    color: unset;
+  }
+
+  article {
+    border: solid 1px;
+    box-shadow: 0 0 4px;
+    margin: 2vh 2vw;
+  }
+
+  header {
+    padding: 10px 10vw;
+  }
+
+  .snippet {
+    position: relative;
+  }
+  .snippet::after {
+    content: "";
+    position: absolute;
+    z-index: 1;
+    bottom: 0;
+    left: 0;
+    pointer-events: none;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 1) 90%
+    );
+    width: 100%;
+    height: 100%;
+  }
+
+  .article-content,
+  .snippet {
+    padding: 10px 5vw;
   }
 </style>
--- src/pages/Blog.svelte
+++ src/pages/Blog.svelte
@@ -1,28 +1,28 @@
 <script>
   import BlogArticle from "../components/BlogArticle.svelte"
+  import BlogTimeline from "../components/BlogTimeline.svelte"
 
   import { default_filter, fetch_blog_articles } from "../blog.js"
   import { onMount } from "svelte"
 
   document.title = document.title + " | Blog"
 
-  // So what may be better is to present each article as a full Blog component
-  // but have the blog component maintain a "collapsed" state.
-  // When the blog is collapsed, it only shows the snippet
-  // When the blog article is clicked, it expands
-
-  // Each blog article also needs to be stored with an "id" and when
-  // the blog is expanded, the window href gets set with the fragment using that
-  // "id"
+  // Its unfortunate we have to use this hacky workaround
+  // But the svelte devs haven't implemented {#each} for asyncGenerator/asyncIterator yet
   onMount(async () => {
-    for await (const each_article of fetch_blog_articles(default_filter)) {
-      new BlogArticle({
-        target: document.getElementsByTagName("main")[0],
-        props: {
-          blog_file_name: each_article.file_name,
-          date: each_article.date,
-        },
-      })
+    if (window.location.hash === "") {
+      for await (const each_article of fetch_blog_articles(default_filter)) {
+        new BlogArticle({
+          target: document.getElementsByTagName("main")[0],
+          props: {
+            blog_file_name: each_article.file_name,
+            date: each_article.date,
+            content: each_article.content,
+          },
+        })
+      }
     }
   })
 </script>
+
+<BlogTimeline />
