11cf812137ff45853166996a925f110ccadb98a4
Nathan Blair <engineer.nblair@gmail.com>
2021-08-02 23:00:53 -0400

    can now load blog snippets
    blogs are written to and read from github (using githu rest api)

---
+++ src/blog.js
@@ -0,0 +1,81 @@
+const github_content_list_endpoint = "https://api.github.com"
+const blog_path = "/repos/nathanblair/blog/contents/"
+
+/** @typedef {Array<{name: string, path: string, download_url: string}>} GitHubPayload */
+/** @typedef {Array<{title: string, date: string, snippet: string}>} BlogSnippet */
+
+/** @param {string} url */
+export async function download_blog_article(url) {
+  return (await fetch(url)).text()
+}
+
+/** Fetch blog articles from the cloud host */
+async function fetch_blog_articles() {
+  /** @type {Response} */
+  let response
+
+  try {
+    response = await fetch(`${github_content_list_endpoint}${blog_path}`)
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+
+  /** @type {GitHubPayload} */
+  let body
+  try {
+    body = await response.json()
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+
+  /** @type {GitHubPayload} */
+  const articles = []
+
+  for (const each_entry of body) {
+    switch (each_entry.name) {
+      case "old":
+      case ".gitignore":
+        break
+
+      default:
+        try {
+          articles.push(each_entry)
+        } catch (err) {
+          console.error(err)
+          continue
+        }
+        break
+    }
+  }
+
+  return articles
+}
+
+export async function parse_blog_articles() {
+  // TODO extract the blog title, date, and generate an excerpt
+  const article_payload = await fetch_blog_articles()
+  /** @type {BlogSnippet} */
+  const articles = []
+
+  const parser = new DOMParser()
+
+  for (const each_article_payload of article_payload) {
+    const article_html = await download_blog_article(
+      each_article_payload.download_url
+    )
+
+    const article_element = parser.parseFromString(article_html, "text/html")
+
+    articles.push({
+      title:
+        article_element.querySelector("article > header > h1")?.innerHTML || "",
+      date:
+        article_element.querySelector("article > header > h2")?.innerHTML || "",
+      snippet: article_element.querySelector("article > p")?.innerHTML || "",
+    })
+  }
+
+  return articles
+}
--- src/pages/Blog.svelte
+++ src/pages/Blog.svelte
@@ -1,5 +1,27 @@
 <script>
+  // import BlogArticle from "../components/BlogArticle.svelte"
+  import BlogSnippet from "../components/BlogSnippet.svelte"
+
+  import { parse_blog_articles } from "../blog.js"
+
   document.title = document.title + " | Blog"
+
+  // TODO The initial page load loads up a snippet of all blog posts (or a curated list somehow?)
+
+  // How do we then make it so loading an individual blog article can appropriately
+  // change the url so it can work with SEO?
 </script>
 
-Coming soon! (TM)
+{#await parse_blog_articles() then articles}
+  {#each articles as each_article}
+    <BlogSnippet
+      blog_title={each_article.title}
+      blog_date={each_article.date}
+      snippet={each_article.snippet}
+    />
+  {/each}
+{/await}
+
+<!-- <svelte:component this={BlogArticle}>
+  {@html each_article}
+</svelte:component> -->
