31645fdb1694512e64f29e97cf767220d78829ea
Nathan Blair <engineer.nblair@gmail.com>
2021-08-05 12:57:47 -0400

    this is actually a pretty sweet implementation
    for blog placeholders
    
    I might try out a couple different transition styles but
    overally this effect is definitely what I was going for

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
 export const page_default_title = "Nathan Blair"
 export const main_id = "main"
+
+export const transition_opacity_class_name = "transition-opacity"
+export const transition_flyout_class_name = "transition-flyout"
+export const transition_flyin_class_name = "transition-flyin"
