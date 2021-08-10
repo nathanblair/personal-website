7e9c7c8e4ca96b8ce15d8d2eb203f4e278f01347
Nathan Blair <engineer.nblair@gmail.com>
2021-08-10 13:20:29 -0400

    more progress on converting to get tree call

--- src/blog.js
+++ src/blog.js
@@ -4,26 +4,30 @@ export const total_days = 31
 export const blog_placeholder_class_name = "blog-placeholder"
 
 const cloud_host_endpoint = "https://api.github.com"
-const blog_path = "/repos/nathanblair/blog/contents/"
-const cloud_host_blog_list_endpoint = `${cloud_host_endpoint}${blog_path}`
+const tree_path = "/repos/nathanblair/blog/git/trees/default/"
+const cloud_host_blog_tree_endpoint = `${cloud_host_endpoint}${tree_path}`
 
 /**
- * @typedef {Array<{
- *  body: string,
- *  body_text: string,
- *  body_html: string,
- *  name: string,
- *  path: string,
- *  type: "file" | "dir" | "symlink" | "submodule",
- *  download_url: string
- * }>} Payload
+ * @typedef {{
+ * path: string,
+ * mode: string,
+ * type: "tree" | "blob",
+ * sha: string,
+ * url: string,
+ * }} Tree
+ */
+
+/**
+ * @typedef {{
+ *  sha: string,
+ *  url: string,
+ *  tree: Array<Tree>,
+ * }} Payload
  */
 
 /** @typedef {[string, string, string]} ArticleDateArray */
 
 /** @typedef {{file_name: string, date: ArticleDateArray, content: string}} Article */
-
-/** @typedef {Map<number, Article>} Articles */
 
 export const DateMap = Object.freeze({
   0: "January",
@@ -67,24 +71,11 @@ export function default_filter() {
  * @returns {AsyncGenerator<Article>}
  */
 export async function* fetch_blog_articles(filter) {
-  // FIXME Replace all this with just calling the GitHub get trees API
-  // So that we can get all the files in the repository
-  // Then we'll just filter through the file paths locally, and populate a list
-  // of articles locally.
-
-  // Then for each article in the local articles array, we'll call the get contents
-  // API for that article, making sure to pass in the appropriate header for
-  // whether the file is a markdown or html file
-
   /** @type {Response} */
   let response
 
   try {
-    response = await fetch(`${cloud_host_blog_list_endpoint}${filter()}`, {
-      headers: new Headers({
-        accept: "application/vnd.github.VERSION.full+json",
-      }),
-    })
+    response = await fetch(`${cloud_host_blog_tree_endpoint}`)
   } catch (err) {
     console.error(err)
     return
@@ -99,42 +90,39 @@ export async function* fetch_blog_articles(filter) {
     return
   }
 
-  if (!Array.isArray(body)) {
+  if (!Array.isArray(body.tree)) {
     return
   }
 
-  for (const each_entry of body) {
-    switch (each_entry.body.type) {
-      case "dir":
-        yield* fetch_blog_articles(() => each_entry.path)
-        continue
-      case "file":
-        const date_array = each_entry.path.split("/")
-        date_array.pop()
-        try {
-          yield {
-            file_name: each_entry.name,
-            // @ts-ignore
-            date: date_array,
-            content:
-              each_entry.path.split(".").pop() === "md"
-                ? each_entry.body_html
-                : each_entry.body_text,
-            // content: await (
-            //   await fetch(cloud_host_blog_list_endpoint + each_entry.path, {
-            //     headers: new Headers({
-            //       accept: `application/vnd.github.VERSION.${
-            //         each_entry.path.split(".").pop() === "md" ? "html" : "raw"
-            //       }`,
-            //     }),
-            //   })
-            // ).text(),
-          }
-        } catch (err) {
-          console.error(err)
-          continue
-        }
-        break
+  const articles = body.tree.filter((each_tree) => {
+    each_tree.path.indexOf(filter()) >= 0 && each_tree.type === "blob"
+  })
+
+  for (const each_article of articles) {
+    const date_array = each_article.path.split("/")
+    date_array.pop()
+    try {
+      yield {
+        file_name: each_article.name,
+        // @ts-ignore
+        date: date_array,
+        content:
+          each_article.path.split(".").pop() === "md"
+            ? each_article.body_html
+            : each_article.body_text,
+        // content: await (
+        //   await fetch(cloud_host_blog_list_endpoint + each_entry.path, {
+        //     headers: new Headers({
+        //       accept: `application/vnd.github.VERSION.${
+        //         each_entry.path.split(".").pop() === "md" ? "html" : "raw"
+        //       }`,
+        //     }),
+        //   })
+        // ).text(),
+      }
+    } catch (err) {
+      console.error(err)
+      continue
     }
   }
   return
