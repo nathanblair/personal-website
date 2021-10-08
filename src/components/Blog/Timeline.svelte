40fe3359badabd224ceab3bf0dad956ee1d92451
Nathan Blair <engineer.nblair@gmail.com>
2021-10-07 23:49:47 -0400

    gets us a decent label going
    still needs styling
    
    and the math to correctly offset it
    
    and the height of the svg element needs properly set

--- src/components/Blog/Timeline.svelte
+++ src/components/Blog/Timeline.svelte
@@ -17,12 +17,25 @@
     selection_applied_callback(tree, filter)
   }
 
+  /** @param {string} tree_path */
+  function extract_date(tree_path) {
+    return tree_path.split("/", 3).join("/")
+  }
+
+  /** @param {string} tree_path */
+  function extract_title(tree_path) {
+    return tree_path.split("/")[3].split("-").join(" ").split(".").slice(0, -1)
+  }
+
   const line_stroke_width = 1
+
+  const timeline_max_height = 300
 
   const entry_spacing = 25
   const entry_stroke_width = 0.5
   const entry_width = 2
   const entry_height = 5
+  const text_padding = 1
 
   const timeline_viewbox_width =
     (tree.length - 1) * entry_spacing + entry_width + entry_stroke_width
@@ -41,6 +54,7 @@
     width={`${
       timeline_viewbox_width * (document.documentElement.clientWidth / 100)
     }px`}
+    height={timeline_max_height}
     class="timeline-graphic"
   >
     <line
@@ -51,7 +65,7 @@
       stroke="black"
       stroke-width={line_stroke_width}
     />
-    {#each tree as _each_entry, each_index}
+    {#each tree as each_entry, each_index}
       <!-- TODO Along svg line element, insert clickable svg box elements -->
       <!-- for each blog entry -->
       <!-- Show the date of each blog entry above the clicable svg box elements -->
@@ -65,7 +79,15 @@
         stroke-width={entry_stroke_width}
         stroke="black"
       />
-      <!-- <text x={entry_spacing * each_index}>{each_entry.path}</text> -->
+      <text
+        x={entry_spacing * each_index}
+        y={text_padding + entry_stroke_width / 2}
+        transform={`rotate(-5, ${entry_spacing * each_index}, ${text_padding})`}
+        fill="black"
+      >
+        <tspan>{extract_date(each_entry.path)}</tspan>
+        <tspan>{extract_title(each_entry.path)}</tspan>
+      </text>
     {/each}
   </svg>
 </div>
@@ -84,6 +106,10 @@
 
   .timeline-graphic {
     display: block;
+  }
+
+  svg text {
+    font-size: 0.09rem;
   }
 
   @media only screen and (min-width: 600px) {
