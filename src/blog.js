a7c8afe9ae50ab9eae7413587249056ced27d267
Nathan Blair <nathanblair@silver.lan>
2021-11-21 12:40:11 -0500

    reverse chronological order blog timeline sorting

--- src/blog.js
+++ src/blog.js
@@ -36,18 +36,18 @@ function is_valid_article(each_entry) {
 }
 
 export const DateMap = Object.freeze({
-  0: "January",
-  1: "February",
-  2: "March",
-  3: "April",
-  4: "May",
-  5: "June",
-  6: "July",
-  7: "August",
-  8: "September",
-  9: "October",
-  10: "November",
-  11: "December",
+  "January": 0,
+  "February": 1,
+  "March": 2,
+  "April": 3,
+  "May": 4,
+  "June": 5,
+  "July": 6,
+  "August": 7,
+  "September": 8,
+  "October": 9,
+  "November": 10,
+  "December": 11,
 })
 
 export function set_blog_page_default_title() {
