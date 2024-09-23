8bd42a1fa263d7effc684d2d5c59e31554befe60
Nathan Blair <me@nathanblair.rocks>
2024-09-23 06:19:45 -0400

    try using process env for cloudflare runtime

--- src/lib/server/github.js
+++ src/lib/server/github.js
@@ -1,9 +1,9 @@
-import { env } from "$env/dynamic/private"
+import { env as dynamic_env } from "$env/dynamic/private"
 import { App } from 'octokit'
 
 export async function get_github_app() {
-  const app_id = env.GITHUB_APP_CLIENT_ID || ""
-  const private_key = env.GITHUB_APP_SECRET || ""
+  const app_id = dynamic_env.GITHUB_APP_CLIENT_ID || process.env.GITHUB_APP_CLIENT_ID || ""
+  const private_key = dynamic_env.GITHUB_APP_SECRET || process.env.GITHUB_APP_SECRET || ""
 
   if (app_id === "") {
     throw new Error("GitHub app ID not found")
@@ -16,9 +16,9 @@ export async function get_github_app() {
 
 /** @param {App} github_app */
 export async function get_github_installation_octokit(github_app) {
-  const install_id = env.GITHUB_APP_INSTALL_ID || ""
+  const install_id = dynamic_env.GITHUB_APP_INSTALL_ID || process.env.GITHUB_APP_INSTALL_ID || ""
 
-  if (github_app === null || install_id === "") {
+  if (install_id === "") {
     throw new Error("GitHub app or installation ID not found")
   }
 
@@ -30,12 +30,10 @@ export async function get_github_repo(github_app) {
   let repository_owner = ""
   let repository_name = ""
 
-  if (github_app) {
-    await github_app.eachRepository(each => {
-      repository_owner = each.repository.full_name.split('/')[0]
-      repository_name = each.repository.full_name.split('/')[1]
-    })
-  }
+  await github_app.eachRepository(each => {
+    repository_owner = each.repository.full_name.split('/')[0]
+    repository_name = each.repository.full_name.split('/')[1]
+  })
 
   return { repository_owner, repository_name }
 }
